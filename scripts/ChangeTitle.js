aed45517cd1e4ffd7e4af430e339a3540c12f4b3
Swapnil Jagadale <swap10june@gmail.com>
2024-09-02 17:03:52 +0200

    Add files via upload

---
+++ scripts/ChangeTitle.js
@@ -0,0 +1,33 @@
+const baseURL = "./../titles/"
+
+function changeLinktoShirts(){
+    console.log("working");
+    var frame = document.getElementById("products-frame");
+    frame.src = `${baseURL}Shirts.html`
+}
+//function changeLinktoPants(){
+//    console.log("working");
+//    var frame = document.getElementById("products-frame");
+//	alert(`${baseURL}Pants.html`);
+//    frame.src = Pants.html;
+//}
+function changeLinktoSummerCollections(){
+    console.log("working");
+    var frame = document.getElementById("products-frame");
+    frame.src = `${baseURL}SummerCollections.html`
+}
+function changeLinktoWinterCollections(){
+    console.log("working");
+    var frame = document.getElementById("products-frame");
+    frame.src = `${baseURL}WinterCollections.html`
+}
+function changeLinktoWatches(){
+    console.log("working");
+    var frame = document.getElementById("products-frame");
+    frame.src = `${baseURL}Watches.html`
+}
+function changeLinktoSunGlasses(){
+    console.log("working");
+    var frame = document.getElementById("products-frame");
+    frame.src = `${baseURL}SunGlasses.html`
+}
